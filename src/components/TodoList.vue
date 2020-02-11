56ff50af4bbdf59621fa09c81e4af699823339b7
kudo <yosuke.kudo910@gmail.com>
2020-02-11 13:10:18 +0900

    Remove todos from $data

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -55,11 +55,7 @@ export default {
     return {
       inputText: "",
       selected: "all",
-      todos: [
-        { id: 1, text: "Learn JavaScript", done: true },
-        { id: 2, text: "Learn Vue", done: false },
-        { id: 3, text: "Build something awesome", done: false }
-      ]
+      todos: []
     };
   },
   computed: {
@@ -110,17 +106,20 @@ export default {
     },
     onBeforeUnload() {
       localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
+    getTodosFromLocalStorage() {
+      try {
+        const data = JSON.parse(localStorage.getItem("todos"));
+        if (data) this.todos = data;
+      } catch (error) {
+        console.log("Something wrong is happened in your stored data.");
+        console.log("Use default data.");
+      }
     }
   },
   created() {
     window.addEventListener("beforeunload", this.onBeforeUnload);
-    try {
-      const data = JSON.parse(localStorage.getItem("todos"));
-      if (data) this.todos = data;
-    } catch (error) {
-      console.log("Something wrong is happened in your stored data.");
-      console.log("Use default data.");
-    }
+    this.getTodosFromLocalStorage();
   }
 };
 </script>
